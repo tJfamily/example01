b6d2b01ab5884e97110ab3d40f13bf6dfc1da383
tJfamily <903983266@qq.com>
2017-10-30 21:54:56 +0800

    add methods to click menu links

--- environment.py
+++ environment.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from codesrepo.base import BDBase
 
 
 def get_driver(context):
@@ -7,6 +8,7 @@ def get_driver(context):
 
 def before_all(context):
     context.driver = get_driver(context)
+    context.bd = BDBase(context.driver)
 
 
 def after_all(context):
--- steps/example01.py
+++ steps/example01.py
@@ -1,30 +1,62 @@
 # -*- coding: utf-8 -*-
 
 
-from behave import when, then, given
+from behave import when, then, given, model
 import time
 
 
-# @when('Open baidu website')
+menu_links = {
+    "新闻": "news",
+    "hao123": "hao123",
+    "地图": "map",
+    "视频": "v",
+    "学术": "xueshu"
+}
+
+
 @when(u"打开{text}")
 def open_baidu(context, text):
     if text == '百度':
-        context.driver.get("http://www.baidu.com")
+        context.bd.go_to_bdhomepage()
 
 
 @then('baidu is open')
 def check_baidu_is_open(context):
-    assert 'baidu' in context.driver.current_url
+    assert 'baidu' in context.bd.get_current_url()
 
 
 @when('search behave')
 def search_behave(context):
-    context.driver.find_element_by_id('kw').send_keys('behave')
+    context.bd.driver.find_element_by_id('kw').send_keys('behave')
     context.searchItem = 'behave'
     time.sleep(5)
 
 
 @then('search result gets return')
 def check_search_result(context):
-    result = context.driver.find_element_by_class_name('result-op')
+    result = context.bd.driver.find_element_by_class_name('result-op')
     assert result
+
+
+@when('open menu {menu_item}')
+def open_menu(context, menu_item):
+    # context.bd.hover_on_more_products()
+    context.bd.click_menu_item(menu_item)
+    context.current_url = context.bd.get_current_url()
+    context.bd.driver.back()
+    time.sleep(2)
+
+
+@then('the right menu link {menu_item} is open')
+def check_current_url(context, menu_item):
+    # remove the white spaces in the heading and tailing part of the string {menu_item}
+    # because the Chinese character will be added a whitespace before passing to the parameter menu_item
+    menu_item = menu_item.strip()
+    assert menu_links[menu_item] in context.current_url
+
+
+@when('open baidu')
+def open_baidu_using_table(context):
+    for row in context.table:
+      # need to update
+        model.add_user(item_link=row['item_link'], link_page=row['link_page'])
